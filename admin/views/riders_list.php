bd940702e6a66ff923bf225b46491f8f8863bb29
wanzanggmail <wanzang@gmail.com>
2026-08-13 10:31:38 +0900

    텍스트 수정

--- admin/views/riders_list.php
+++ admin/views/riders_list.php
@@ -712,17 +712,3 @@ $agencyOptions   = $isAgencyCreator ? [] : Organization::agencyOptions();
 </script>
 
 <?php require_once INC_PATH . '/app_content_close.php'; ?>
-
-<?php require_once INC_PATH . '/app_content_close.php'; ?>
-
-<?php require_once INC_PATH . '/app_content_close.php'; ?>
-
-<?php require_once INC_PATH . '/app_content_close.php'; ?>
-
-<?php require_once INC_PATH . '/app_content_close.php'; ?>
-
-<?php require_once INC_PATH . '/app_content_close.php'; ?>
-
-<?php require_once INC_PATH . '/app_content_close.php'; ?>
-
-<?php require_once INC_PATH . '/app_content_close.php'; ?>
